4e755c9ee42e215dfa0856efcb659cd31ad884c5
Igor Spasić <igor@jodd.org>
2018-12-12 20:24:41 +0100

    Removed Trello

--- static/trello/js/client.js
+++
@@ -1,8 +0,0 @@
-TrelloPowerUp.initialize({
-  'card-buttons': function(t, options) {
-    return [{
-      icon: 'https://heapspace.rs/apple-icon-144x144.png',
-      text: 'Done!',
-    }];
-  },
-});
